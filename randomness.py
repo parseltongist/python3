caad52751979785e102c456b2a7c81218655f007
parseltongist <alex.milinkevich@gmail.com>
2021-06-15 13:44:03 +0200

    stage 3/4 with a prediction module

--- randomness.py
+++ randomness.py
@@ -1,6 +1,7 @@
+import math
 final_string = ''
 triadas = ["000", "001", "010", "011", "100", "101", "110", "111"]
-pattern_dict = dict.fromkeys(triadas, [])
+pattern_dict = dict.fromkeys(triadas, [0, 0])
 
 
 while len(final_string) < 100:
@@ -13,10 +14,7 @@ while len(final_string) < 100:
     else:
         pass
 else:
-    print("\nFinal data string:\n{}\n".format(final_string))
-
-
-
+    print("\nFinal data string:\n{}\n\n".format(final_string))
 
 
 for key in triadas:
@@ -24,7 +22,29 @@ for key in triadas:
     pattern_dict[key] = [final_string.count(x),final_string.count(y)]
 
 
-#print("test is",test)
+def prediction():
+    global pattern_dict
+    test_string = input("Please enter a test string containing 0 or 1:\n\n")
+    #prediction_string = test_string[:3]  # default value as it can be any at this stage
+    prediction_string = "101"
+    total_len = len(test_string)
+    for number in range(total_len-3):  # decreasing by 3 as the first three random symbols are already set and we still need not to exceed the length of the line
+        triad = test_string[number:number + 3]
+        if pattern_dict[triad][0] >= pattern_dict[triad][1]:
+            prediction_string += '0'
+        else:
+            prediction_string += '1'
+        # or prediction_string += '0' if pattern_dict[triad][0] >= pattern_dict[triad][1] else '1'
+    correctly_predicted = 0
+    for x in range(3, total_len):  # issue was here, as we need to count correct matches starting from number 3 only
+        if prediction_string[x] == test_string[x]:
+            correctly_predicted += 1
+    print(f"prediction:\n{prediction_string}\n")
+    print(f"Computer guessed right {correctly_predicted} out of {total_len} symbols ({(correctly_predicted / (total_len -3) * 100):.2f} %)")  # total_len -3 here because we are not interested in all symbols but without first three.
 
-for key in pattern_dict:
-    print(f"{key}: {pattern_dict[key][0]},{pattern_dict[key][1]}")
+
+
+prediction()
+# we do not print at this stage
+#for key in pattern_dict:
+#    print(f"{key}: {pattern_dict[key][0]},{pattern_dict[key][1]}")
